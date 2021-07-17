5d8dd5adfe03763392c443194cca61feb6145a40
2tle <iam@2tle.io>
2021-07-18 03:36:44 +0900

    fragment edited

---
+++ app/src/main/java/com/example/a2021sunlinhackathon/Data/DonationData.java
@@ -0,0 +1,81 @@
+package com.example.a2021sunlinhackathon.Data;
+
+public class DonationData {
+    public String ldName;
+    public String ldPhotoUrl;
+    public String ldOutUrl;
+    public String ldExp;
+
+    public String rdName;
+    public String rdPhotoUrl;
+    public String rdOutUrl;
+    public String rdExp;
+
+    public DonationData() {
+
+    }
+
+    public String getLdName() {
+        return ldName;
+    }
+
+    public void setLdName(String ldName) {
+        this.ldName = ldName;
+    }
+
+    public String getLdPhotoUrl() {
+        return ldPhotoUrl;
+    }
+
+    public void setLdPhotoUrl(String ldPhotoUrl) {
+        this.ldPhotoUrl = ldPhotoUrl;
+    }
+
+    public String getLdOutUrl() {
+        return ldOutUrl;
+    }
+
+    public void setLdOutUrl(String ldOutUrl) {
+        this.ldOutUrl = ldOutUrl;
+    }
+
+    public String getLdExp() {
+        return ldExp;
+    }
+
+    public void setLdExp(String ldExp) {
+        this.ldExp = ldExp;
+    }
+
+    public String getRdName() {
+        return rdName;
+    }
+
+    public void setRdName(String rdName) {
+        this.rdName = rdName;
+    }
+
+    public String getRdPhotoUrl() {
+        return rdPhotoUrl;
+    }
+
+    public void setRdPhotoUrl(String rdPhotoUrl) {
+        this.rdPhotoUrl = rdPhotoUrl;
+    }
+
+    public String getRdOutUrl() {
+        return rdOutUrl;
+    }
+
+    public void setRdOutUrl(String rdOutUrl) {
+        this.rdOutUrl = rdOutUrl;
+    }
+
+    public String getRdExp() {
+        return rdExp;
+    }
+
+    public void setRdExp(String rdExp) {
+        this.rdExp = rdExp;
+    }
+}
