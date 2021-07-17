5869294d80d191cc4abdd67e6305368083d96cdb
2tle <iam@2tle.io>
2021-07-18 04:08:42 +0900

    donationIntent

--- app/src/main/java/com/example/a2021sunlinhackathon/Activity/DonationActivity.java
+++ app/src/main/java/com/example/a2021sunlinhackathon/Activity/DonationActivity.java
@@ -2,9 +2,13 @@ package com.example.a2021sunlinhackathon.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.example.a2021sunlinhackathon.R;
+import com.example.a2021sunlinhackathon.databinding.ActivityDonationBinding;
 
 public class DonationActivity extends AppCompatActivity {
 
@@ -12,5 +16,26 @@ public class DonationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
+        ActivityDonationBinding binding = ActivityDonationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.doName.setText(getIntent().getExtras().getString("name"));
+        binding.doExp.setText(getIntent().getExtras().getString("exp"));
+        Glide.with(this).load(getIntent().getExtras().getString("imgUrl")).into(binding.doImg);
+        binding.doBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+            }
+        });
+        binding.goDoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getBaseContext(), WedviewActivity.class);
+                it.putExtra("url",getIntent().getExtras().getString("url"));
+                startActivity(it);
+            }
+        });
+
     }
 }
