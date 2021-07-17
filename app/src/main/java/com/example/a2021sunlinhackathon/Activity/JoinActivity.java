162442faa09eced53a6e229136880b3bb6af5aa4
2tle <iam@2tle.io>
2021-07-18 06:58:20 +0900

    fp

--- app/src/main/java/com/example/a2021sunlinhackathon/Activity/JoinActivity.java
+++ app/src/main/java/com/example/a2021sunlinhackathon/Activity/JoinActivity.java
@@ -182,7 +182,7 @@ public class JoinActivity extends AppCompatActivity {
                                                 userModel.id = id;
                                                 userModel.name = name;
                                                 userModel.profileImageUrl = imageUrl;
-                                                userModel.flant= (int)(Math.random()*9)+1;
+                                                userModel.plant= (int)(Math.random()*9)+1;
                                                 userModel.uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
                                                 FirebaseDatabase.getInstance().getReference().child("UserProfile").child(uid).setValue(userModel).addOnSuccessListener(new OnSuccessListener<Void>() {
